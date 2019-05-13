cf9f3057db50a8efb02e5d6f84c69d4201007436
Michael Maggs <michaelmaggs@gmail.com>
2019-05-13 20:50:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
   else
     katz_deli.each do |name|
       puts "Currently serving #{katz_deli[0]}."
-      katz_deli = katz_deli.shift
+      katz_deli.shift
     end
   end
 end
